d3b937f2aa2a597eb19bb7a99973a2700d239946
LMFront-end <perfilgitlinamaria@gmail.com>
2022-02-22 18:11:59 -0500

    se agregan rutas para pruebas en postman

--- src/main/java/co/com/sofka/katafullStack/controllers/TodoController.java
+++ src/main/java/co/com/sofka/katafullStack/controllers/TodoController.java
@@ -10,6 +10,8 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
+// ruta de proyecto de react
+@CrossOrigin(origins = "http://localhost:3000")
 public class TodoController {
 
     /*
@@ -23,30 +25,35 @@ public class TodoController {
     private TodoServices todoServices;
 
     // Mostrar todas las tareas
+    // http://localhost:8080/api/todos
     @GetMapping("/todos")
     public List<Todo> listTodo(){
         return todoServices.listTodo();
     }
 
     // buscar por id
+    // http://localhost:8080/api/todo/{id}
     @GetMapping("/todo/{id}")
     public Optional<Todo> findById(@PathVariable Long id) {
         return todoServices.findById(id);
     }
-    
+
     // Crear una tarea
+    // http://localhost:8080/api/todo/create
     @PostMapping("/todo/create")
     public Todo save(@RequestBody Todo todo){
         return todoServices.save(todo);
     }
 
     // Actualizar una tarea
+    // http://localhost:8080/api/todo/update
     @PutMapping("/todo/update")
     public Todo updateTodo(@RequestBody Todo todo){
         return todoServices.updateTodo(todo);
     }
 
     // Borrar una tarea
+    // http://localhost:8080/api/delete/{id}
     @DeleteMapping("/delete/{id}")
     public void deleteById(@PathVariable Long id){
         todoServices.deleteById(id);
